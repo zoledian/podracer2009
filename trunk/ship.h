f2596847a63937f421c5fb1d4e9bc95ee98c2f58
stefan.vendel <stefan.vendel@473dd6ce-146e-11de-babb-ddc7aea8d8a1>
2009-05-14 23:01:07 +0000

    jumping and moving ship, sortof. Press m to jump if space doesn't work (for now)

--- trunk/ship.h
+++ trunk/ship.h
@@ -1,18 +1,21 @@
 #ifndef SHIP_H
 #define SHIP_H
 
+#include "camera.h"
 #include <GL/glut.h>
 
 class Ship {
  public:
   Ship();
-  void drawShip();  
+  void drawShip(Camera* cam);
   void moveHere(GLint cubeNr); // cube nr 1 - 5
+  void jumpShip();
 
  private:
   void turn();
   void turnRotateZ(GLboolean movingLeft, GLfloat distanceLeft);
   void hover();
+  void jump();
   void drawBody();
   void drawWindshield();
 
@@ -24,6 +27,9 @@ class Ship {
   GLboolean turning;
   GLint hoverY;
   GLint angleZ;
+  GLint angleX;
+  GLfloat jumpDestination;
+  GLfloat jumpHeight;
 };
 
 #endif
