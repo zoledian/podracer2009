5c1dba98bd07043eadc77be3ad15284195e78bab
thomas@etma.se <thomas@etma.se@473dd6ce-146e-11de-babb-ddc7aea8d8a1>
2009-05-14 23:02:04 +0000

    added forgotten zpr files to the repository

---
+++ trunk/zpr.h
@@ -0,0 +1,36 @@
+/* This code is taken from LAB3 in the course TSBK07, linköpings university.
+   Thanks to the author Ingemar Ragnemalm */
+
+#ifndef ZPR_H
+#define ZPR_H
+
+/*
+ * Zoom-pan-rotate mouse manipulation module for GLUT
+ *
+ * Originally written by Nigel Stewart
+ *
+ * Hacked quite a bit by Mikael Kalms
+ */
+
+#ifdef WIN32
+#include <windows.h>
+#endif
+
+#include <GL/glut.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+void zprInit();
+GLdouble* zprGetObjectMatrix();
+GLdouble* zprGetLightMatrix();
+GLdouble* zprGetCameraMatrix();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
